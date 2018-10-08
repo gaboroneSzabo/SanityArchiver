76fd0166d48479382b943501540aa0651e5b1776
gaboroneszabo <puma678@gmail.com>
2018-10-08 16:23:23 +0200

    add basic file browser logic

---
+++ SanityArchiver/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SanityArchiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.rightPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(12, 425);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(28, 13);
+            this.pathLabel.TabIndex = 2;
+            this.pathLabel.Text = "path";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(46, 422);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.ReadOnly = true;
+            this.pathTextBox.Size = new System.Drawing.Size(661, 20);
+            this.pathTextBox.TabIndex = 3;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.AutoScroll = true;
+            this.leftPanel.Location = new System.Drawing.Point(12, 12);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(307, 383);
+            this.leftPanel.TabIndex = 6;
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.AutoScroll = true;
+            this.rightPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.rightPanel.Location = new System.Drawing.Point(371, 12);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(400, 172);
+            this.rightPanel.TabIndex = 7;
+            this.rightPanel.WrapContents = false;
+            // 
+            // sortButton
+            // 
+            this.sortButton.Location = new System.Drawing.Point(703, 308);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(75, 23);
+            this.sortButton.TabIndex = 8;
+            this.sortButton.Text = "sort";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.rightPanel);
+            this.Controls.Add(this.leftPanel);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "File Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.FlowLayoutPanel rightPanel;
+        private System.Windows.Forms.Button sortButton;
+    }
+}
+
---
+++ SanityArchiver/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchiver
+{
+    public partial class Form1 : Form
+    {
+        List<DirectoryInfo> directories = new List<DirectoryInfo>();
+        List<FileInfo> files = new List<FileInfo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:\\");
+            
+            foreach(DirectoryInfo directory in di.GetDirectories())
+            {
+                directories.Add(directory);
+                Label dirLabel = new Label();
+                dirLabel.Text = directory.Name;
+                
+                rightPanel.Controls.Add(dirLabel);
+                
+            }
+            foreach(FileInfo file in di.GetFiles())
+            {
+                files.Add(file);
+                Label fileLabel = new Label();
+                fileLabel.Text = file.Name;
+               
+                rightPanel.Controls.Add(fileLabel);
+               
+            }
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+    }
+}
